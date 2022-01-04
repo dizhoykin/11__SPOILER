80f4b87be8f9799577a70cfeca6da8d6941257cf
dizhoykin <dizhoykin@gmail.com>
2022-01-04 21:47:43 +0700

    Done

--- STARTER-FILES/app.js
+++ STARTER-FILES/app.js
@@ -2,19 +2,6 @@ const questionsList = document.querySelectorAll('li');
 
 for (let i = 0; i < questionsList.length; i++) {
   questionsList[i].addEventListener('click', () => {
-    questionsList[i].classList.add('expand');
-    const pseudoElement = questionsList[i].querySelector('.question');
-    const styles = window.getComputedStyle(pseudoElement, ':after');
-    // const closeBtn = url('./images/close.svg');
-    styles['background-image'] = 1;
-
+      questionsList[i].classList.toggle('expand');
   });
-  // questionsList[i].addEventListener('mouseover', () => {
-  //   questionsList[i].classList.add('expand');
-  // // });
-  // questionsList[i].addEventListener('mouseout', () => {
-  //   questionsList[i].classList.remove('expand');
-  // });
-
-
-};
+}
